abb4d398e21edd964542084e0fe5651b715865c0
abrochard <roxas@localhost.localdomain>
2014-10-28 12:45:48 -0400

    add set proba neighbors

--- AI.c
+++ AI.c
@@ -15,3 +15,12 @@ int count_neighbors(int **grid, int size_x, int size_y, int i, int j)
 	else
 		return 3;
 }
+
+/* 
+ * Given the value returned by a shot at (i,j)
+ * Sets/Increase the probability of the neighbors
+*/
+void set_neighbors_prob(int **grid, int size_x, int size_y, int i, int j, int value)
+{
+
+}
--- AI.h
+++ AI.h
@@ -2,5 +2,6 @@
 #define AI_H
 
 int count_neighbors(int **grid, int size_x, int size_y, int i, int j);
+void set_neighbors_prob(int **grid, int size_x, int size_y, int i, int j, int value);
 
 #endif

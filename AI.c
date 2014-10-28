b1cbdf606d4f02d0e26e423bf7c098ff8f9c7b57
abrochard <roxas@localhost.localdomain>
2014-10-28 11:43:29 -0400

    add count_neighbors fct

--- AI.c
+++ AI.c
@@ -2,4 +2,16 @@
 #include "AI.h"
 #include "grid.h"
 
-
+int count_neighbors(int **grid, int size_x, int size_y, int i, int j)
+{
+	if (i<0 || i>=size_x || j<0 || j>=size_y)
+		return -1;
+	else if (i>0 && j>0 && i<size_x-1 && j<size_y-1)
+		return 8;
+	else if (i>0 && i<size_x-1 && (j==0 || j==size_y-1))
+		return 5;
+	else if (j>0 && j<size_y-1 && (i==0 || i==size_x-1))
+		return 5;
+	else
+		return 3;
+}
--- AI.h
+++ AI.h
@@ -1,6 +1,6 @@
 #ifndef AI_H
 #define AI_H
 
-
+int count_neighbors(int **grid, int size_x, int size_y, int i, int j);
 
 #endif

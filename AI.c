0cf0855383664c1c36811833aaefb9d0dbd99055
abrochard <abrochard@ymail.com>
2014-11-18 13:44:57 -0500

    float support for proba table

--- AI.c
+++ AI.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "AI.h"
 #include "grid.h"
 
@@ -33,7 +34,7 @@ int count_neighbors(float **grid, int size_x, int size_y, int i, int j)
  * Given the value returned by a shot at (i,j)
  * Sets/Increase the probability of the neighbors
 */
-void set_neighbors_prob(float **grid, int size_x, int size_y, int i, int j, int value)
+void set_neighbors_proba(float **grid, int size_x, int size_y, int i, int j, int value)
 {
 	int nbr_neighbors = count_neighbors(grid, size_x, size_y, i, j);
 	float proba = (float)value/nbr_neighbors;
@@ -55,8 +56,50 @@ void set_neighbors_prob(float **grid, int size_x, int size_y, int i, int j, int 
 		grid[i+1][j+1] += proba;
 }
 
+float **init_proba_grid(int x, int y)
+{
+	float **grid = malloc(sizeof(float *)*y);
+	if (grid == NULL) {
+		perror("malloc returned null");
+		exit(1);
+	}
+	int i;
+	for (i=0; i<y; i++) {
+		grid[i] = malloc(sizeof(float)*x);
+		if (grid[i] == NULL) {
+			perror("malloc returned null");
+			exit(1);
+		}
+	}
+	return grid;
+}
+
+void free_proba_grid(float **grid, int x, int y)
+{
+	int i;
+	for (i=0; i<y; i++) {
+		free(grid[i]);
+	}
+	free(grid);
+}
+
+void write_proba_grid(float **grid, int x, int y, float value)
+{
+	int i,j;
+	for (i=0; i<y; i++) {
+		for (j=0;j<x; j++) {
+			grid[i][j]=value;
+		}
+	}
+}
+
 void init_AI(int size_x, int size_y, int mines)
 {
-	int **grid = init_grid(size_x, size_y);
-	write_grid(grid, size_x, size_y, -1);
+	struct Proba_Board pb;
+	pb.size_x = size_x;
+	pb.size_y = size_y;
+	pb.mines = mines;
+	pb.grid = init_proba_grid(pb.size_x, pb.size_y);
+	write_proba_grid(pb.grid, pb.size_x, pb.size_y, -1);
+	free_proba_grid(pb.grid, pb.size_x, pb.size_y);
 }
--- AI.h
+++ AI.h
@@ -1,15 +1,23 @@
 #ifndef AI_H
 #define AI_H
 
+struct Point {
+	int x;
+	int y;
+};
+
 struct Proba_Board {
 	float **grid;
 	int size_x;
 	int size_y;
 	int mines;
-}
+};
 
 int count_neighbors(float **grid, int size_x, int size_y, int i, int j);
-void set_neighbors_prob(float **grid, int size_x, int size_y, int i, int j, int value);
+void set_neighbors_proba(float **grid, int size_x, int size_y, int i, int j, int value);
+float **init_proba_grid(int x, int y);
+void free_proba_grid(float **grid, int x, int y);
+void write_proba_grid(float **grid, int x, int y, float value);
 void init_AI(int size_x, int size_y, int mines);
 
 #endif

b500b985851b592c1535709a79380fa0d53af28c
abrochard <aaa.brochard@gmail.com>
2014-12-27 18:10:17 +0100

    new marking for AI data

--- AI.c
+++ AI.c
@@ -5,10 +5,24 @@
 #include "AI.h"
 #include "grid.h"
 
+/*
+ * Contains a probability/weight grid as floats
+ * A higher proba = higher chances to be a mine
+ * A negative number or 0 indicates the result of a shot: 
+ * -2 -> 2 adjacent mines
+ * 0 -> no adjacent mines
+ * Except:
+ * -9 means no chances to be a mine and should be targetted next
+ * -10 stands for blank points (points with no info/proba)
+ */ 
+
+#define ZERO -9
+#define BLANK -10
+
 struct Proba_Board pb;
 
 /*
- * Return the number of non-zero neighbors
+ * Return the number of non-shot neighbors
  */
 int count_neighbors(float **grid, int size_x, int size_y, int i, int j)
 {
@@ -16,32 +30,32 @@ int count_neighbors(float **grid, int size_x, int size_y, int i, int j)
 		return -1;
 	int sum=0;
 	if (i>0)
-		sum += (grid[i-1][j]!=0 && grid[i-1][j]!=-1);
+		sum += (grid[i-1][j]>0 || grid[i-1][j]==BLANK);
 	if (i<size_x-1)
-		sum += (grid[i+1][j]!=0 && grid[i+1][j]!=-1);
+		sum += (grid[i+1][j]>0 || grid[i+1][j]==BLANK);
 	if (j>0)
-		sum += (grid[i][j-1]!=0 && grid[i][j-1]!=-1);
+		sum += (grid[i][j-1]>0 || grid[i][j-1]==BLANK);
 	if (j<size_y-1)
-		sum += (grid[i][j+1]!=0 && grid[i][j+1]!=-1);
+		sum += (grid[i][j+1]>0 || grid[i][j+1]==BLANK);
 	if (i>0 && j>0)
-		sum += (grid[i-1][j-1]!=0 && grid[i-1][j-1]!=-1);
+		sum += (grid[i-1][j-1]>0 || grid[i-1][j-1]==BLANK);
 	if (i>0 && j<size_y-1)
-		sum += (grid[i-1][j+1]!=0 && grid[i-1][j+1]!=-1);
+		sum += (grid[i-1][j+1]>0 || grid[i-1][j+1]==BLANK);
 	if (i<size_x-1 && j>0)
-		sum += (grid[i+1][j-1]!=0 && grid[i+1][j-1]!=-1);
+		sum += (grid[i+1][j-1]>0 || grid[i+1][j-1]==BLANK);
 	if (i<size_x-1 && j<size_y-1)
-		sum += (grid[i+1][j+1]!=0 && grid[i+1][j+1]!=-1);
+		sum += (grid[i+1][j+1]>0 || grid[i+1][j+1]==BLANK);
 	return sum;
 }
 
 float set_proba(float f, float proba)
 {
-	if (f==0 || f==-1)
+	if (f<=0 && f!=BLANK)
 		return f;
-	else if (f==2)
+	else if (f==BLANK)
 		return proba;
 	else if (proba==0)
-		return 0;
+		return ZERO;
 	else
 		return f+proba;
 }
@@ -111,14 +125,17 @@ void write_proba_grid(float **grid, int x, int y, float value)
 
 void init_AI(int size_x, int size_y, int mines)
 {
+	srand(time(NULL));
 	pb.size_x = size_x;
 	pb.size_y = size_y;
 	pb.mines = mines;
 	pb.grid = init_proba_grid(pb.size_x, pb.size_y);
-	write_proba_grid(pb.grid, pb.size_x, pb.size_y, 2);
-	srand(time(NULL));
+	write_proba_grid(pb.grid, pb.size_x, pb.size_y, BLANK);
 }
 
+/*
+ *Returns the first point with zero chances to be a mine
+ */
 struct Point get_point_zero()
 {
 	struct Point target;
@@ -126,7 +143,7 @@ struct Point get_point_zero()
 	int i,j;
 	for (i=0; i<pb.size_x; i++) {
 		for (j=0; j<pb.size_y; j++) {
-			if(pb.grid[i][j]==0) {
+			if(pb.grid[i][j]==ZERO) {
 				target.x=i;
 				target.y=j;
 				break;
@@ -136,41 +153,45 @@ struct Point get_point_zero()
 	return target;
 }
 
+/*
+ * Returns the point on the grid with the lowest proba
+ * (excludes blank points)
+ */
 struct Point get_lowest_proba_point()
 {
 	struct Point target;
-	target.x = -1;
 	int i,j;
-	float min = 2;
+	float min = 0;
 	for (i=0; i<pb.size_x; i++) {
 		for (j=0; j<pb.size_y; j++) {
-			if(pb.grid[i][j]<min && pb.grid[i][j]>0) {
+			if((pb.grid[i][j]<min || min==0)&& pb.grid[i][j]>0) {
 				target.x=i;
 				target.y=j;
 				min=pb.grid[i][j];
 			}
 		}
 	}
+	if (min==0)
+		target.x = -1;
 	return target;
 }
 
 struct Point get_random_point(int risky)
 {
-	//if risky is not 1
-	//by random, I mean random among the blank ones
-	//excluding neighbors of shot points
+	//if risky is not 1, random means random among the blank ones
+	//excluding neighbors of shot points (points with proba)
 	//if risky is 1,
 	//random among all the non-shot ones
 	int size=0;
 	struct Point *list = malloc(sizeof(struct Point)*pb.size_x*pb.size_y);
 	if(list==NULL) {
-		fprintf(stderr, "malloc failed");
+		perror("malloc failed");
 		exit(1);
 	}
 	int i,j;
 	for (i=0; i<pb.size_x; i++) {
 		for (j=0; j<pb.size_y; j++) {
-			if(pb.grid[i][j]==2 || (risky && pb.grid[i][j]!=-1 && pb.grid[i][j]!=0)) {
+			if(pb.grid[i][j]==BLANK || (risky && pb.grid[i][j]>0)) {
 				list[size].x=i;
 				list[size].y=j;
 				size++;
@@ -188,35 +209,30 @@ struct Point get_random_point(int risky)
 
 struct Point AI_get_target()
 {
+	//refine all weights with new data
+	//..
 	struct Point target;
 	//look for points at proba 0
 	target = get_point_zero();
-	if (target.x != -1) {
-		//mark as shot
-		pb.grid[target.x][target.y]=-1;
+	if (target.x != -1)
 		return target;	
-	}
+	//pick random among blank points
+	target = get_random_point(0);
+	if (target.x != -1)
+		return target;	
 	//look for point with lowest proba
 	target = get_lowest_proba_point();
-	if (target.x != -1) {
-		pb.grid[target.x][target.y]=-1;
+	if (target.x != -1)
 		return target;	
-	}
-	//pick random
-	target = get_random_point(0);
-	if (target.x != -1) {
-		pb.grid[target.x][target.y]=-1;
-		return target;	
-	}
+	//pick any point which wasn't shot (shouldn't happen)
 	target = get_random_point(1);
-	pb.grid[target.x][target.y]=-1;
 	return target;
 }
 
 void AI_send_result(int i, int j, int game)
 {
+	pb.grid[i][j]=-1*game;
 	set_neighbors_proba(pb.grid, pb.size_x, pb.size_y, i, j, game);
-	pb.grid[i][j]=-1;
 }
 
 void free_AI()
